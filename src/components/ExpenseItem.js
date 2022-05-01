c0b0a8339a1df49b8461ce9d67d6370790c93c5c
Extuinger <namanbhai21@gmail.com>
2022-05-01 23:51:13 +0530

    Second Comit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+// const expenseDate = new Date(2021, 2, 28);
+// const expenseTitle = 'Bike Expenses';
+// const expenseAmount = 'Rs 10000'
+
+
+function ExpenseItem(props) {
   return (
     <div className='expense-item'>
-      <h2>April Expenses</h2>
+      <h2>{props.date}</h2>
       <div className='expense-item__description'>
-        <h2>Bike Expenses</h2>
-        <div className='expense-item__price'>Rs. 10000</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>{props.amount}</div>
       </div>
     </div>
   );
